d4e84d1c5b6dcf681422835d0cc1b83480bf01ae
AB498 <abcd49800@gmail.com>
2024-06-25 22:57:09 +0600

    recaptcha key updated

--- .app/plugins/google-recaptcha.ts
+++ .app/plugins/google-recaptcha.ts
@@ -1,13 +1,13 @@
-import { VueReCaptcha } from 'vue-recaptcha-v3';
+import { VueReCaptcha } from 'vue-recaptcha-v3'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueReCaptcha, {
-    siteKey: '6LfcL0EaAAAAAJTW1mDVxCN4a7rEr1jdNqR2HRND',
+    siteKey: '6LctKgEqAAAAAJsPNVQblIBUA081w5P9iL7_glio',
     loaderOptions: {
       autoHideBadge: false,
       explicitRenderParameters: {
         badge: 'bottomright',
       },
     },
-  });
-});
+  })
+})
